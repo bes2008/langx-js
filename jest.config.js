61c4f7ad420ff9c870cc0b2abbc65c8ad0bf9c1a
fangjinuo <fs1194361820@163.com>
2020-03-14 20:58:22 +0800

    * using jest to execute javascript, typescript unit test

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports= {
+    "automock": false,
+    "browser": true,
+    "bail": true,
+    "cacheDirectory": "<rootDir>/src/test/result/cache",
+    "collectCoverage": true,
+    "collectCoverageFrom": ["<rootDir>/src/main"],
+    "coverageDirectory": "<rootDir>/src/test/result/coverage",
+    "coverageReporters": ["json"],
+    "moduleFileExtensions": ["js", "jsx", "json", "ts", "tsx"],
+    "notify": true,
+    "clearMocks": true,
+    "roots":["<rootDir>/src/test"],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    }
+};
