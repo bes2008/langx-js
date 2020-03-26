5f8113cf579a45cbb687a45ae8154bf707109cec
fangjinuo <fs1194361820@163.com>
2020-03-26 17:22:38 +0800

    * refactor code

--- src/main/lodash.ts
+++ src/main/lodash.ts
@@ -3,19 +3,19 @@ import * as Collects from "./Collects";
 import {List} from "./Iterables";
 
 export function chunk(array: Array<any>, size?: number): Array<Array<any>> {
-    let size0 = (size == null) ? 1 : Numbers.parseInt(size);
-    let list: List<List<any>> = Collects.partitionBy(array, {
-        apply(index: number, element: any) {
-            return Numbers.parseInt(index / element) + (index % size0 == 0 ? 1 : 0)
-        }
-    });
+    let size0: number = size == null ? 1 : Numbers.parseInt(size);
     let result: Array<Array<any>> = [];
-    Collects.forEach(list, {
-        accept(ls: List<any>) {
-            result.push(Collects.toArray(ls));
+    let tmp: Array<any>;
+    Collects.forEach(array, {
+        accept(index: number, element: any) {
+            let newArray: boolean = index % size0 == 0;
+            if (newArray) {
+                tmp = [];
+                result.push(tmp);
+            }
+            tmp.push(element);
         }
     });
-
     return result;
 }
 
@@ -27,9 +27,9 @@ export function compact(array: Array<any>) {
     })
 }
 
-export function concat(array:Array<any>, ...a2 ) {
-    let newArray:Array<any> = Array.from(array);
-    Collects.forEach(a2,{
+export function concat(array: Array<any>, ...a2) {
+    let newArray: Array<any> = Array.from(array);
+    Collects.forEach(a2, {
         accept(element: any) {
             newArray.push(element);
         }
