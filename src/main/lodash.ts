7a18027e6e7148a1b603c41c2cc9ad25c00f232b
fangjinuo <fs1194361820@163.com>
2020-04-01 14:51:45 +0800

    * supports find, flatten for an array

--- src/main/lodash.ts
+++ src/main/lodash.ts
@@ -71,14 +71,6 @@ export function differenceWith(array: Array<any>, values: Array<any>, comparator
     }).toArray([]);
 }
 
-export function first(array: Array<any>): any {
-    return Collects.findFirst(array, null);
-}
-
-export function head(array: Array<any>): any {
-    return first(array);
-}
-
 export function indexOf(array: Array<any>, value: any, fromIndex?: number): number {
     fromIndex = (fromIndex == null || fromIndex < 0) ? 0 : Numbers.parseInt(fromIndex);
     if (fromIndex >= array.length) {
@@ -122,51 +114,55 @@ export function dropRight(array: Array<any>, n: number): Array<any> {
     return array;
 }
 
-function _dropCountWhile(array: Array<any>, predicate:string|object|Array<any>|Function):number {
-    Preconditions.checkTrue(Types.isArray(array));
+
+function _buildArrayPredicate(predicate: string | object | Array<any> | Function): Function | Predicate2<any, any> | Predicate<any> {
     Preconditions.checkNonNull(predicate);
-    let p:Function|Predicate2<any, any>|Predicate<any>;
+    let p: Function | Predicate2<any, any> | Predicate<any>;
 
     // [0] property
     // [1] value
-    if(Types.isArray(predicate)){
-        Preconditions.checkTrue((<Array<any>>predicate).length>=2);
+    if (Types.isArray(predicate)) {
+        Preconditions.checkTrue((<Array<any>>predicate).length >= 2);
         Preconditions.checkTrue(Types.isString(predicate[0]));
         let property = predicate[0];
         p = function (element) {
-            return element[<string>property]==predicate[1];
+            return element[<string>property] == predicate[1];
         };
-    }
-    else
+    } else
         // .property
-    if(Types.isString(predicate)){
+    if (Types.isString(predicate)) {
         let property = predicate;
         p = function (element) {
             return element[<string>property];
         };
-    }
-    else if(Functions.isPredicate(predicate)){
+    } else if (Functions.isPredicate(predicate)) {
         p = <Predicate2<any, any>>predicate;
-    }
-    else if(Types.isSimpleObject(predicate)){
+    } else if (Types.isSimpleObject(predicate)) {
         p = function (element) {
             let match = true;
-            for(let entry of new ObjectPropertiesIterator(predicate,ObjectPropertiesIterateType.KEY_VALUE)){
-                if( element[entry.key]!==entry.value){
-                    match =false;
+            for (let entry of new ObjectPropertiesIterator(predicate, ObjectPropertiesIterateType.KEY_VALUE)) {
+                if (element[entry.key] !== entry.value) {
+                    match = false;
                     break;
                 }
             }
             return match;
         };
-    }else{
+    } else {
         p = Functions.truePredicate();
     }
+    return p;
+}
+
+
+function _dropCountWhile(array: Array<any>, predicate: string | object | Array<any> | Function): number {
+    Preconditions.checkTrue(Types.isArray(array));
+    predicate = _buildArrayPredicate(predicate);
 
     let count = 0;
-    let breakPredicate = function(element, index){
-        let b = !Functions.callPredicate(p, element, index, array);
-        if(!b){
+    let breakPredicate = function (element, index) {
+        let b = !Functions.callPredicate(predicate, element, index, array);
+        if (!b) {
             count++;
         }
         return b;
@@ -175,16 +171,96 @@ function _dropCountWhile(array: Array<any>, predicate:string|object|Array<any>|F
     return count;
 }
 
-export function dropWhile(array: Array<any>, predicate:string|object|Array<any>|Function) {
-    let count : number = _dropCountWhile(array, predicate);
-    array.splice(0,count);
+export function dropWhile(array: Array<any>, predicate: string | object | Array<any> | Function) {
+    let count: number = _dropCountWhile(array, predicate);
+    array.splice(0, count);
     return array;
 }
 
-export function dropRightWhile(array: Array<any>, predicate:string|object|Array<any>|Function){
+export function dropRightWhile(array: Array<any>, predicate: string | object | Array<any> | Function) {
     array = array.reverse();
     array = dropWhile(array, predicate);
     return array.reverse();
 }
 
+export function fill(array: Array<any>, value: any, fromIndex: number, toIndex: number): Array<any> {
+    Preconditions.checkTrue(Types.isArray(array));
+    toIndex = toIndex == null || toIndex > array.length ? array.length : toIndex;
+    fromIndex = fromIndex == null || fromIndex < 0 ? 0 : fromIndex;
+    fromIndex = fromIndex > toIndex ? toIndex : fromIndex;
+    while (fromIndex < toIndex) {
+        array[fromIndex] = value;
+    }
+    return array;
+}
 
+
+export function findIndex(array: Array<any>, predicate: string | object | Array<any> | Function): number {
+    predicate = _buildArrayPredicate(predicate);
+    let index = -1;
+    Collects.forEach(array, function (element, i) {
+        index = i;
+    }, <Function>predicate, {
+        test(i: any) {
+            return index !== -1;
+        }
+    });
+    return index;
+}
+
+export function findLastIndex(array: Array<any>, predicate?: string | object | Array<any> | Function): number {
+    predicate = _buildArrayPredicate(predicate ? predicate : truePredicate());
+    let index = -1;
+    Pipeline.of(array).reverse().forEach(function (element, i) {
+        index = i;
+    }, <Function>predicate, {
+        test(i: any) {
+            return index !== -1;
+        }
+    });
+    if (index !== -1) {
+        index = array.length - 1 - index;
+    }
+    return index;
+}
+
+export function first(array: Array<any>): any {
+    return Collects.findFirst(array, null);
+}
+
+export function flatten(array: Array<any> | any): Array<any> {
+    let newArray: Array<any> = [];
+    array = Collects.asList(array).toArray();
+    Collects.forEach(array, (element) => {
+        Collects.addAll(newArray, Collects.asList(element));
+    });
+    return newArray;
+}
+
+export function flattenDeep(array: Array<any> | any): Array<any> {
+    let newArray: Array<any> = [];
+    array = Collects.asList(array).toArray();
+    Collects.forEach(array, (element) => {
+        Collects.addAll(newArray, flattenDeep(element));
+    });
+    return newArray;
+}
+
+export function flattenDepth(array: Array<any> | any, depth?: number, currentDepth?: number) {
+    depth = depth == null || !depth || depth < 1 ? 1 : depth;
+    let current = (currentDepth == null || !currentDepth || currentDepth < -1) ? 0 : currentDepth;
+    array = Collects.asList(array).toArray();
+    if (current < depth) {
+        let newArray: Array<any> = [];
+        Collects.forEach(array, (element) => {
+            current++;
+            Collects.addAll(newArray, flattenDepth(element, depth, current));
+        });
+        return newArray;
+    }
+    return array;
+}
+
+export function head(array: Array<any>): any {
+    return first(array);
+}
